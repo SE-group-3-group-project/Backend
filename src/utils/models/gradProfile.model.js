2c8b2359f15179600906849bb4f8dd295ea511af
alexmercsls <alexhammad@hotmail.co.uk>
2022-12-02 14:26:03 +0000

    gradProfileModel

---
+++ src/utils/models/gradProfile.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+    id_: {type: Number},
+    name: { first: { type: String }, last: { type: String } },
+    
+    contactDetails: { personalEmail: { type: String }, dfEmail: { type: String }, github: { type: String }, linkedIn: { type: String }, phoneNumber: { type: Number } },
+
+    personalStories: [{
+        degrees: { university: { type: String }, degreeSubject: { type: String }, degreeLevel: { type: String }, grade: { type: String }, startDate: { type: Date }, endDate: { type: Date } }, weight: { type: String }, priority: { type: Number }, description: { type: String }
+    }],
+    
+    schoolQualifications: [{
+        school: { type: String }, examType: { type: String }, subject: { type: String }, grade: { type: String }, year: { type: Number }, weight: { type: String }, priority: { type: Number }, description: { type: String }
+    }],
+
+    workExperiences: [{
+        type: { type: String }, employer: { type: String }, position: { type: String }, startDate: { type: Date }, endDate: { type: Date }, weight: { type: String }, priority: { type: Number }, description: { type: String }
+     
+    }],
+
+    personalAchievements: [{
+        type: { type: String }, issuer: { type: String }, award: { type: String }, grade: { type: Number }, year: { type: Number }, weight: { type: String }, priority: { type: Number }, description: { type: String }
+ 
+    }],
+    portfolio: [{
+        title: { type: String }, url: { type: String }, year: { type: Number }, priority: { type: Number }, description: { type: String }
+    }]
+
+    
+
+
+});
+
+
+    
+
+
+    
+    
+        
+
+    
+
+
