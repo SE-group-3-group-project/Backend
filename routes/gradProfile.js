49815cceabbcf0adb15eb4622c5f37999ba646d9
Christopher West <cwestx99@hotmail.co.uk>
2022-12-15 16:22:30 +0000

    Added gradprofile get by email function

--- routes/gradProfile.js
+++ routes/gradProfile.js
@@ -6,20 +6,23 @@ import mongoIdRegExp from "../src/regexps.js";
 export const router = express.Router();
 
 router
-	.route("/:id")
+	.route("/:email")
 	.get((req, res) => {
-		const id = req.params.id;
-		if (!id.match(mongoIdRegExp)) {
-			return res.status(422).json({
-				message: `There was a problem with the Grad profile Id`,
-			});
-		}
-		GradProfile.findById(id)
+		// const id = req.params.id;
+		// if (!id.match(mongoIdRegExp)) {
+		// 	return res.status(422).json({
+		// 		message: `There was a problem with the Grad profile Id`,
+		// 	});
+		// }
+		const email = req.params.email
+		console.log(email)
+		GradProfile.findOne({ personalEmail: email })
 			.then((grad) => {
 				if (grad) {
 					return res.json(grad);
 				} else {
 					return res.status(404).send({ message: "Graduate not found." });
+
 				}
 			})
 			.catch((err) => {
